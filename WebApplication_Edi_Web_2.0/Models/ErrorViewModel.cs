4914d0b3371812eacc6066ebf72566daf4020aea
Jhonatan Mendoza <118300542+IngJhonatanM@users.noreply.github.com>
2024-12-10 09:05:53 -0400

    Delete WebApplication_Edi_Web_2.0 directory
    Because_global_Changes_in_EdiBank

--- WebApplication_Edi_Web_2.0/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication_Edi_Web_2._0.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
